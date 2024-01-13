d6032b3a62f71a285f7b0e7abd9b2f23c4f4c8eb
Benjamin Sam <79420331+bvsam@users.noreply.github.com>
2024-01-13 02:59:07 -0500

    Adding basic window colours

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <SFML/Graphics.hpp>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 int main()
 {
+    sf::VertexArray vertexArray{sf::PrimitiveType::Points, WINDOW_WIDTH * WINDOW_HEIGHT};
+    uint32_t idx = 0;
+    for (uint32_t x{0}; x < WINDOW_WIDTH; ++x)
+    {
+        for (uint32_t y{0}; y < WINDOW_HEIGHT; ++y)
+        {
+            vertexArray[idx].position = sf::Vector2f{static_cast<float>(x), static_cast<float>(y)};
+            vertexArray[idx].color = sf::Color::Green;
+            idx++;
+        }
+    }
+
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Fractal");
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -21,7 +36,7 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        // window.draw(...);
+        window.draw(vertexArray);
 
         // end the current frame
         window.display();
